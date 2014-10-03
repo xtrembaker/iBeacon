6c3f908d0346f09d6055e09e2a44edb4a84cf95c
XtremBaker <xtrembaker@gmail.com>
2014-10-03 20:36:46 +0200

    Rebuild project

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -45,6 +45,62 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        
+        app.startMonitoringBeacon();
+    },
+    startMonitoringBeacon: function(){
+      console.log('START MONITORING BEACON !');
+      var logToDom = function (message) {
+        var e = document.createElement('label');
+        e.innerText = message;
+
+        var br = document.createElement('br');
+        var br2 = document.createElement('br');
+        document.body.appendChild(e);
+        document.body.appendChild(br);
+        document.body.appendChild(br2);
+
+        window.scrollTo(0, window.document.height);
+      };
+
+      var delegate = new cordova.plugins.locationManager.Delegate().implement({
+
+          didDetermineStateForRegion: function (pluginResult) {
+
+              logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+
+              cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+                  + JSON.stringify(pluginResult));
+          },
+
+          didStartMonitoringForRegion: function (pluginResult) {
+              console.log('didStartMonitoringForRegion:', pluginResult);
+
+              logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+          },
+
+          didRangeBeaconsInRegion: function (pluginResult) {
+              logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+          }
+
+      });
+
+      var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9';
+      var identifier = 'beaconOnTheMacBooksShelf';
+      var minor = 1000;
+      var major = 5;
+      var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+      cordova.plugins.locationManager.setDelegate(delegate);
+
+      // required in iOS 8+
+      cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
+      // or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+      cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+          .fail(console.error)
+          .done();
+
     }
 };
 
